c1c39064f918e160bf78c5d775567afdd6820d6a
CesarHRM <eng.cesarh@gmail.com>
2021-11-25 23:32:13 -0300

    css

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 var iframe = document.getElementById("tela"); 
-var URLs = ['https://g1.globo.com/', 'https://globo.com/']
+var URLs = ['https://app.powerbi.com/view?r=eyJrIjoiNDIyYWJjM2ItYTgwMi00NjQzLWEzYTItZWMwNzIzOTY2MDUxIiwidCI6IjA0ZWM2MTA5LTRjNzktNGM3My1hZTcxLWE0NzRjMDlhMWY1YSJ9',
+                'https://app.powerbi.com/view?r=eyJrIjoiYjNlNzAzMGQtZGQ0Yi00ZTQyLTg1NmMtZTdiZmI3MTAzZTQzIiwidCI6IjUzMjFhYTk0LWFmODgtNDU3Zi1hNWRkLTBkNWNhM2ZjYzIzOSIsImMiOjEwfQ%3D%3D',
+                    'https://app.powerbi.com/view?r=eyJrIjoiMWU1OTliMzAtMDQxZi00NmRlLWFjZDMtNDY4NTA2N2I0MjYxIiwidCI6IjU3NzI4ZTBjLTNjMjMtNDMwMC05ZGViLTk1NTc5ODFkMDc4MSJ9'];  //   JSON.parse(localStorage.getItem('links')) || 
 var urlAtual= URLs[1];
 
-var intervalo = 12000;
+var intervalo = 10000;
+
+var ciclo = "";
 
 function troca_link(url){   
     iframe.src = url
@@ -23,19 +27,33 @@ function trocarTela(){
 
 function teste(){console.log('testwe');}
 
+function iniciar(){
+    ciclo = setInterval(trocarTela,intervalo);
+}
 
-setInterval(trocarTela,intervalo);
-
+function parar(){
+    clearInterval(ciclo);
+}
 
 function addLink(){
-    console.log('bot press');
+    //console.log('bot press');
     const inputUrl = document.querySelector("#botAdd");
     const valorUrl = inputUrl.value;
-    console.log(valorUrl);
-
+    //console.log(valorUrl);
     URLs.push(valorUrl);
+    salvarDadosNoStorage();
 }
 
 function setarTempo(){
     
+    const inputtemp = document.querySelector("#botTemp");
+    const valortemp = inputtemp.value;
+    intervalo = valortemp * 1000;
+    console.log(intervalo);
+    
 }
+
+function salvarDadosNoStorage(){
+    localStorage.setItem('links', JSON.stringify(URLs) );
+}
+
